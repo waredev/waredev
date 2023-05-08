e0475c6f228f4c5fbfc73fd6596d2d85b3950a05
Andi Firwansyah <macbook@AndiFirwansyah2.lan>
2023-05-08 08:48:11 +0700

    update seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,34 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  // head: {
+  //   title: 'Waredev Digital Solution',
+  //   htmlAttrs: {
+  //     lang: 'id'
+  //   },
+  //   meta: [
+  //     { charset: 'utf-8' },
+  //     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  //     { hid: 'description', name: 'description', content: 'Empowering Your Business with Innovative Software Solutions.' },
+  //     { name: 'format-detection', content: 'telephone=6282193758418' }
+  //   ],
+  //   link: [
+  //     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+  //     // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap' },
+  //   ]
+  // },
   head: {
-    title: 'Waredev Digital Solution',
-    htmlAttrs: {
-      lang: 'id'
-    },
+    title: 'Waredev Digital Solution | Custom Software Development',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Empowering Your Business with Innovative Software Solutions.' },
-      { name: 'format-detection', content: 'telephone=6282193758418' }
+      { hid: 'og:title', name: 'og:title', content: 'Waredev Digital Solution | Custom Software Development' },
+      { hid: 'og:description', name: 'og:description', content: 'Empowering Your Business with Innovative Software Solutions.' },
+      { hid: 'og:image', name: 'og:image', content: '/assets/images/logo.png' },
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'keywords', name: 'keywords', content: 'waredev, waredev digital solution, waredev.co.id, waredev custom software development, custom software development, CV. Waredev Digital Solution' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap' },
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
