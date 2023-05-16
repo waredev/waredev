d625fe6656b481d10b6ec14e68a39d00034ac0e3
Andi Firwansyah <macbook@AndiFirwansyah2.lan>
2023-05-16 11:34:48 +0700

    update menu link and mobile version;

---
+++ pages/blog/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="pt-32 font-nunito">
+    <div class="mb-24 xl:px-20 px-10">
+      <div class="font-bold text-md text-word text-3xl">Blog page is under development.</div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'Blog Page'
+}
+</script>
---
+++ pages/portfolio/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="pt-32 font-nunito">
+    <div class="mb-24 xl:px-20 px-10">
+      <div class="font-bold text-md text-word text-3xl">Portfolio</div>
+      <div class="text-md text-light mt-4">
+        Every product we build has a different story. We join our clients'
+        <br />
+        teams as a tech partner to overcome challenges and help them grow.
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'Portfolio Page'
+}
+</script>
