a1c814b84ed7f414f9347232670baf0595decaf7
andifirwansyah <andifirwansyah250@gmail.com>
2023-06-05 17:29:45 +0700

    team

--- pages/about-us/index.vue
+++ pages/about-us/index.vue
@@ -12,26 +12,47 @@
       <div class="font-bold text-md text-word text-3xl">
         - Head of Team
       </div>
-      <div class="grid lg:grid-cols-3 grid-cols-none grid-rows-3 lg:grid-rows-none py-10">
+      <div class="grid lg:grid-cols-3 grid-cols-2 gap-5 py-10">
         <div class="flex flex-col items-center">
-          <div class="w-72 mb-5">
-              <img src="~assets/images/teams/dudi.jpg" class="w-72 h-80 object-cover rounded-md" />
+          <div class="lg:w-72 w-auto mb-5">
+            <img src="~assets/images/teams/dudi.jpg" class="lg:w-72 lg:h-80 w-full h-48 object-cover rounded-md" />
             <div class="text-word font-nunito font-bold text-xl mt-5">Dudi Ali Murtado</div>
             <div class="text-word font-nunito text-sm mt-2">Chief Technology Officer</div>
           </div>
         </div>
         <div class="flex flex-col items-center">
-          <div class="w-72 mb-5">
-            <img src="~assets/images/teams/andi.jpg" class="w-72 h-80 object-cover rounded-md" />
+          <div class="lg:w-72 w-auto mb-5">
+            <img src="~assets/images/teams/andi.jpg" class="lg:w-72 lg:h-80 w-full h-48 object-cover rounded-md" />
             <div class="text-word font-nunito font-bold text-xl mt-5">Andi Firwansyah</div>
             <div class="text-word font-nunito text-sm mt-2">Chief Executive Officer</div>
           </div>
         </div>
         <div class="flex flex-col items-center">
-          <div class="w-72 mb-5">
-            <img src="~assets/images/teams/pranto.jpg" class="w-72 h-80 object-cover rounded-md" />
+          <div class="lg:w-72 w-auto mb-5">
+            <img src="~assets/images/teams/pranto.jpg" class="lg:w-72 lg:h-80 w-full h-48 object-cover rounded-md" />
             <div class="text-word font-nunito font-bold text-xl mt-5">Pranto Suwarno</div>
             <div class="text-word font-nunito text-sm mt-2">Chief Marketing Officer</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-light-blue xl:px-20 px-10 py-20">
+      <div class="font-bold text-md text-word text-3xl">
+        - The Engineer
+      </div>
+      <div class="grid lg:grid-cols-3 grid-cols-2 gap-5 py-10">
+        <div class="flex flex-col items-center">
+          <div class="lg:w-72 w-auto mb-5">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" class="lg:w-72 lg:h-80 w-full h-48 object-cover rounded-md" />
+            <div class="text-word font-nunito font-bold text-xl mt-5">Ichwan Setyawan</div>
+            <div class="text-word font-nunito text-sm mt-2">Front End Developer</div>
+          </div>
+        </div>
+        <div class="flex flex-col items-center">
+          <div class="lg:w-72 w-auto mb-5">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" class="lg:w-72 lg:h-80 w-full h-48 object-cover rounded-md" />
+            <div class="text-word font-nunito font-bold text-xl mt-5">Herry Legowo</div>
+            <div class="text-word font-nunito text-sm mt-2">Junior Developer</div>
           </div>
         </div>
       </div>
